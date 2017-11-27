5dc319428c5cbb4c282b9d4fa7ba8db114aebfb3
ubuntu-yk <smileyk11@foxmail.com>
2017-11-27 03:13:04 -0800

    fix error

--- model.py
+++ model.py
@@ -4,29 +4,29 @@ from gensim.models.word2vec import LineSentence
 
 import os
 origin_path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+def w(model, target):
+	try:
+		tup = model.similar_by_word(target)
+	except:
+		raise
+	for i in tup:
+		print(i[0], ' ', i[1])
 
-print('Load Sentences...')
-cut_dir = origin_path+'/word2vec_zh/cut_data/'
-sentences = LineSentence(cut_dir+'data_small.csv')
+def train_embedding():
+    print('Load Sentences...')
+    cut_dir = origin_path+'/word2vec_zh/cut_data/'
+    sentences = LineSentence(cut_dir+'data_small.csv')
 
-print('Initial Model...')
-model = Word2Vec(sentences, min_count=30, size=200, workers=4)
+    print('Initial Model...')
+    model = Word2Vec(sentences, min_count=30, size=200, workers=4)
 
-model.save('my.model')
+    model.save('my.model')
 
-
-
-
-# for file_name in files_name:
-# 	if file_name[0]=='.':
-# 		continue
-# 	print(file_name)
-# 	#file_name = '东方.txt'
-# 	final_sentences += LineSentence(cut_dir+file_name)
-# 	if not vocab_builded:
-# 		model.build_vocab(sentences)
-# 		vocab_builded = True
-# 	model.train(sentences)
+def test_embedding(target):
+    cut_dir = origin_path + '/word2vec_zh/'
+    model = Word2Vec.load(cut_dir+'my.model')
+    w(model, target)
 
 
-	
+if __name__ == '__main__':
+    test_embedding(u'爱情')
