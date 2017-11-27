ffa17b5fd2039020a15b94f7e5c921dc45bf6705
ubuntu-yk <smileyk11@foxmail.com>
2017-11-27 04:53:08 -0800

    fix error

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
 # -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# coding:utf-8
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
 import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 origin_path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 def w(model, target):
 	try:
@@ -10,23 +15,26 @@ def w(model, target):
 	except:
 		raise
 	for i in tup:
-		print(i[0], ' ', i[1])
+		print i[0], ' ', i[1]
 
 def train_embedding():
     print('Load Sentences...')
     cut_dir = origin_path+'/word2vec_zh/cut_data/'
-    sentences = LineSentence(cut_dir+'data_small.csv')
+    sentences = LineSentence(cut_dir+'data_chusai_fusai.csv')
 
     print('Initial Model...')
     model = Word2Vec(sentences, min_count=30, size=200, workers=4)
 
     model.save('my.model')
 
-def test_embedding(target):
+def testt_embedding(target):
     cut_dir = origin_path + '/word2vec_zh/'
     model = Word2Vec.load(cut_dir+'my.model')
     w(model, target)
+    print model[target]
 
 
 if __name__ == '__main__':
-    test_embedding(u'爱情')
+    train_embedding()
+    testt_embedding(u'爱情')
+
